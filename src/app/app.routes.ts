72fbf0414fb3aa46bc68c38f394deaf55076774d
OGM HOLDING GROUP <ogmholdinggroup@gmail.com>
2025-12-03 16:27:03 +0200

    add log in page with responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { ForgetPasswordComponent } from './auth/forget-password/forget-password.component';
 
-export const routes: Routes = [{ path: 'login', component: LoginComponent }];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'change-password', component: ForgetPasswordComponent },
+];
