a3307c98022b2840d7c1486e90a3f5623878440d
OGM HOLDING GROUP <ogmholdinggroup@gmail.com>
2025-12-03 12:40:49 +0200

    add log in page with responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       colors: {
         "custom-gray": "#7c7b84",
+        "custom-blue": "#001A36",
+        "custom-baby-blue": "#0060AB",
       },
     },
   },
